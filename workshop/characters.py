cf4c5333952ea5165f563a70371e89c093e71b77
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2024-11-06 18:07:46 +0700

    Added documentation in characters.py

--- workshop/characters.py
+++ workshop/characters.py
@@ -1,5 +1,23 @@
 from core.model import Character
 # Yep, go ahead, make a new character here~
+
+# This is where you make characters~
+# The most important part is the outfit and id
+# Both are used to handle character sprite location you see???
+
+# In minecraft, character sprite will be located in
+# textures/character.id/character.outfit/emotion.png
+
+# Emotion is defined in the dialogue file.
+# Like uhh... show(cupa,"happy")
+
+# It will translate to 
+# textures/cupa/default/happy.png
+
+# Currently the Minecraft mod still hasn't have Character Outfit Swapping, yet
+# But it's a good groundwork to have
+# Just to make your script futureproof~
+
 Cupa = Character(
         id="cupa", # The Id is for folder structure and such, make it unique pls, lower case
         name = "Cupa", # This shows up on the dialogue box
@@ -7,7 +25,6 @@ Cupa = Character(
         thoughts= "Unknown", # Same as above
         outfit = "default", # This is the outfit folder
         states = None, # Unused for now
-        maxhealth = 40, # Using Minecraft data would be better I think... Eh, is fine
         traits = None
     )
 
@@ -18,9 +35,6 @@ Andr = Character(
         thoughts= "Unknown",
         outfit = "default",
         states = None,
-        maxhealth = 40,
         traits = None,
-
-
 )
 
