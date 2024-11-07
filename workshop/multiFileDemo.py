99905f4f2db87ea5ab3fe89821137c5d4dc289de
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2024-11-07 08:38:42 +0700

    Added more comments

--- workshop/multiFileDemo.py
+++ workshop/multiFileDemo.py
@@ -7,14 +7,13 @@ from core.compiler import compile
 # If you're into organization, this is how you go about it.
 # This file is the 'core' file
 
-# Import your scripts (all the .py file inside the script folder)
-from script import script1,script2,script3
-
 vn = VisualNovelModule() # Your Module~ Always need this~
 storyName = "cupa" # This will be the name of the Json File
 
-# This is the list of your script.
 
+# Import your scripts (all the .py file inside the script folder)
+
+from script import script1,script2,script3
 scripts = [
     script1.story(),
     script2.story(),
@@ -23,7 +22,9 @@ scripts = [
 ]
 
 # Yes, it's that simple
-# I have Overengineering Phobia
+# No need to change anything below.
+
+
 def compileMultiStory():
     for script in scripts:
         vn.dialogueDict+=script
